7618813e0f9e95d24d1c35f1ca72d8c3c432634d
modcomlearning <modcomlearning@gmail.com>
2023-07-06 10:26:45 +0300

    First Commit

---
+++ app/src/main/java/com/modcom/medilabsapp/models/LabTests.kt
@@ -0,0 +1,13 @@
+package com.modcom.medilabsapp.models
+
+class LabTests (
+        val availability: String = "",
+        val lab_id: String = "",
+        val more_info: String = "",
+        val reg_date: String = "",
+        val test_description: String = "",//1
+        val test_discount: String = "",
+        val test_cost: String = "",//3
+        val test_id: String = "",
+        val test_name: String = "",//2
+        )
