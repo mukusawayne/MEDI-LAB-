3e6081db368dc7d8d5ed9a82288f21edfc837af8
modcomlearning <modcomlearning@gmail.com>
2023-07-11 11:13:54 +0300

    First Commit

--- app/src/main/java/com/modcom/medilabsapp/SingleLabTest.kt
+++ app/src/main/java/com/modcom/medilabsapp/SingleLabTest.kt
@@ -1,19 +1,17 @@
 package com.modcom.medilabsapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.textview.MaterialTextView
 
 class SingleLabTest : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single_lab_test)
         //find the textView
-        val tvtest_id = findViewById<MaterialTextView>(R.id.test_id)
-        //Get test id value from Intent Extras
         val test_id = intent.extras?.getString("test_id")
-        //put the test id value to tvtestid
-        tvtest_id.text = test_id
         //=============
         val tvtest_name = findViewById<MaterialTextView>(R.id.test_name)
         val test_name = intent.extras?.getString("test_name")
@@ -21,11 +19,11 @@ class SingleLabTest : AppCompatActivity() {
         //==============
         val tvtest_cost = findViewById<MaterialTextView>(R.id.test_cost)
         val test_cost = intent.extras?.getString("test_cost")
-        tvtest_cost.text = test_cost
+        tvtest_cost.text = "$test_cost KES"
         //==============
         val tvtest_discount = findViewById<MaterialTextView>(R.id.test_discount)
         val test_discount = intent.extras?.getString("test_discount")
-        tvtest_discount.text = test_discount
+        tvtest_discount.text = "$test_discount % OFF"
         //==============
         val tvtest_description = findViewById<MaterialTextView>(R.id.test_description)
         val test_description = intent.extras?.getString("test_description")
