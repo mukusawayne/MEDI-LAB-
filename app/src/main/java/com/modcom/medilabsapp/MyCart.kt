429b1f11a05c5fe4a83fb16bf97082a795903140
modcomlearning <modcomlearning@gmail.com>
2023-07-13 12:08:03 +0300

    First Commit

--- app/src/main/java/com/modcom/medilabsapp/MyCart.kt
+++ app/src/main/java/com/modcom/medilabsapp/MyCart.kt
@@ -2,10 +2,35 @@ package com.modcom.medilabsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textview.MaterialTextView
+import com.modcom.medilabsapp.adapters.LabTestsCartAdapter
+import com.modcom.medilabsapp.helpers.SQLiteCartHelper
 
 class MyCart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_cart)
+        //put total cost in a textview
+        val total = findViewById<MaterialTextView>(R.id.total)
+        val helper = SQLiteCartHelper(applicationContext)
+        total.text = "Total: "+helper.totalCost()
+
+        //Find recycler
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        recycler.layoutManager = LinearLayoutManager(applicationContext)
+        recycler.setHasFixedSize(true)
+        //Access adapter and provide it with from getAllItems
+        if(helper.getNumItems() == 0){
+            Toast.makeText(applicationContext, "Your Cart is Empty",
+                Toast.LENGTH_SHORT).show()
+        }
+        else {
+            val adapter = LabTestsCartAdapter(applicationContext)
+            adapter.setListItems(helper.getAllItems())//pass data
+            recycler.adapter = adapter //link adapter to recycler
+        }
     }
 }
