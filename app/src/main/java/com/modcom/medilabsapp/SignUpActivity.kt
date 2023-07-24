38b45da63f5939300ee118822ed969638405cc9e
modcomlearning <modcomlearning@gmail.com>
2023-07-24 10:39:02 +0300

    Second Commit

--- app/src/main/java/com/modcom/medilabsapp/SignUpActivity.kt
+++ app/src/main/java/com/modcom/medilabsapp/SignUpActivity.kt
@@ -3,30 +3,48 @@ package com.modcom.medilabsapp
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.DatePicker
-import android.widget.EditText
+import android.widget.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var buttonDatePicker: Button
     private lateinit var editTextDate: EditText
+    private lateinit var spinner: Spinner
+    private lateinit var selectedItemText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+
+
         buttonDatePicker = findViewById(R.id.buttonDatePicker)
         editTextDate = findViewById(R.id.editTextDate)
 
         buttonDatePicker.setOnClickListener {
             showDatePickerDialog()
         }//end onclick
+
+        //Spinner
+        spinner = findViewById(R.id.spinner)
+        selectedItemText = findViewById(R.id.selectedItemText)
+        // Sample data for the spinner
+        val data = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
+        // Create an ArrayAdapter using the sample data
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, data)
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Set the adapter to the spinner
+        spinner.adapter = adapter
+
+
+
+
     }//end oncreate
 
     //other functions
     private fun showDatePickerDialog() {
         val calendar = Calendar.getInstance()
-
         // Create a date picker dialog and set the current date as the default selection
         val datePickerDialog = DatePickerDialog(
             this,
