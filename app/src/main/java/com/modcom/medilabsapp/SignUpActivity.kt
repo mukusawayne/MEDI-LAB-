5caad98e133882b1c48040dfe7e676335c361794
modcomlearning <modcomlearning@gmail.com>
2023-07-24 10:04:44 +0300

    Second Commit

--- app/src/main/java/com/modcom/medilabsapp/SignUpActivity.kt
+++ app/src/main/java/com/modcom/medilabsapp/SignUpActivity.kt
@@ -1,28 +1,53 @@
 package com.modcom.medilabsapp
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.EditText
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var buttonDatePicker: Button
+    private lateinit var editTextDate: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        buttonDatePicker = findViewById(R.id.buttonDatePicker)
+        editTextDate = findViewById(R.id.editTextDate)
 
-        /*
-what we will be Posting
-        {
-    "surname": "Ann005",
-    "others": "Doe",
-    "gender": "Female",
-    "email": "ann005@gmail.com",
-    "phone": "+254729225710",
-    "dob": "2000-08-05",
-    "password": "qweRTY123",
-    "location_id": "1"
-}
+        buttonDatePicker.setOnClickListener {
+            showDatePickerDialog()
+        }//end onclick
+    }//end oncreate
 
-         */
+    //other functions
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+
+        // Create a date picker dialog and set the current date as the default selection
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _: DatePicker, year: Int, month: Int, day: Int ->
+                val selectedDate = formatDate(year, month, day)
+                editTextDate.setText(selectedDate)
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        // Show the date picker dialog
+        datePickerDialog.show()
+    }
+
+    private fun formatDate(year: Int, month: Int, day: Int): String {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, day)
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        return dateFormat.format(calendar.time)
     }
 
 
-}
+}//end class
