8bde45023a7f85d6bd2152b815a4d1535af4a707
modcomlearning <modcomlearning@gmail.com>
2023-07-26 10:07:04 +0300

    Second Commit

--- app/src/main/java/com/modcom/medilabsapp/SignInActivity.kt
+++ app/src/main/java/com/modcom/medilabsapp/SignInActivity.kt
@@ -1,10 +1,12 @@
 package com.modcom.medilabsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textview.MaterialTextView
 import com.modcom.medilabsapp.constants.Constants
 import com.modcom.medilabsapp.helpers.ApiHelper
 import com.modcom.medilabsapp.helpers.PrefsHelper
@@ -15,6 +17,14 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        //link to register while in Login.
+        val linktoregister = findViewById<MaterialTextView>(R.id.linktoregister)
+        linktoregister.setOnClickListener {
+            startActivity(Intent(applicationContext, SignUpActivity::class.java))
+        }
+
+
         val surname = findViewById<TextInputEditText>(R.id.surname)
         val password = findViewById<TextInputEditText>(R.id.password)
 
@@ -45,8 +55,25 @@ class SignInActivity : AppCompatActivity() {
                         PrefsHelper.savePrefs(applicationContext,
                             "access_token", access_token)
 
+                        //convert message to an Object
+                        val member = JSONObject(message)
+                        val member_id = member.getString("member_id")
+                        val email = member.getString("email")
+                        val surname = member.getString("surname")
+
                         PrefsHelper.savePrefs(applicationContext,
                             "userObject", message)
+
+                        PrefsHelper.savePrefs(applicationContext,
+                            "member_id", member_id)
+
+                        PrefsHelper.savePrefs(applicationContext,
+                            "email", email)
+
+                        PrefsHelper.savePrefs(applicationContext,
+                            "surname", surname)
+
+
                         //Proceed to HomePage. We need to Create It.
 
                     }
