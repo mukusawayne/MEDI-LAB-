7281cdb6818cf21aee3b65b8a666b361a678bcaa
modcomlearning <modcomlearning@gmail.com>
2023-07-27 10:54:46 +0300

    Second Commit

--- app/src/main/java/com/modcom/medilabsapp/ui/dependants/DependantFragment.kt
+++ app/src/main/java/com/modcom/medilabsapp/ui/dependants/DependantFragment.kt
@@ -1,18 +1,28 @@
 package com.modcom.medilabsapp.ui.dependants
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.RadioButton
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import com.modcom.medilabsapp.MainActivity
 import com.modcom.medilabsapp.R
 import com.modcom.medilabsapp.helpers.PrefsHelper
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class DependantFragment : Fragment() {
-
+    private lateinit var buttonDatePicker: Button
+    private lateinit var editTextDate: EditText
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,9 +30,60 @@ class DependantFragment : Fragment() {
         // Inflate the layout for this fragment
         val root = inflater.inflate(R.layout.fragment_dependant, container, false)
         //Code here
+        buttonDatePicker = root.findViewById(R.id.buttonDatePicker)
+        editTextDate = root.findViewById(R.id.editTextDate)
+
+        buttonDatePicker.setOnClickListener {
+            showDatePickerDialog()
+        }//end onclick
+
+        //post to API
+        val adddependant = root.findViewById<MaterialButton>(R.id.adddependant)
+        adddependant.setOnClickListener {
+            //Push/Post data to APi.
+            val surname = root.findViewById<TextInputEditText>(R.id.surname)
+            val others = root.findViewById<TextInputEditText>(R.id.others)
+            val female = root.findViewById<RadioButton>(R.id.radioFemale)
+            val male = root.findViewById<RadioButton>(R.id.radioMale)
+            var gender = "N/A"
+            if (female.isSelected) {
+                gender = "Female"
+            }
+            if (male.isSelected) {
+                gender = "Male"
+            }
+
+
+
+
+        }//end listener
 
         return root
 
-    }
+    }//end oncreate view
 
-}
+
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        // Create a date picker dialog and set the current date as the default selection
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { _: DatePicker, year: Int, month: Int, day: Int ->
+                val selectedDate = formatDate(year, month, day)
+                editTextDate.setText(selectedDate)
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        // Show the date picker dialog
+        datePickerDialog.show()
+    }
+    private fun formatDate(year: Int, month: Int, day: Int): String {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, day)
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        return dateFormat.format(calendar.time)
+    }
+}//last brace
