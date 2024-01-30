db3f0401e286034de4f7c2bcd0678044970da24c
Wayne <mukusawayne@gmail.com>
2024-01-30 12:41:58 +0300

    Third commit

--- app/src/main/java/com/modcom/medilabsapp/constants/Constants.kt
+++ app/src/main/java/com/modcom/medilabsapp/constants/Constants.kt
@@ -1,6 +1,6 @@
 package com.modcom.medilabsapp.constants
 class Constants {
     companion object {
-        val BASE_URL = "https://modcom.pythonanywhere.com/api"
+        val BASE_URL = "https://waynemukusa.pythonanywhere.com/api"
     }
 }
